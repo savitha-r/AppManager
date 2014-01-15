d528fc43d4b4fef3516f8ebd1d95f4069bf94ef5
savitha-r <savitha.r@2359media.com>
2014-01-15 19:54:11 +0530

    basic CRUD companies

--- app/controllers/super_admin/companies_controller.rb
+++ app/controllers/super_admin/companies_controller.rb
@@ -4,4 +4,33 @@ class SuperAdmin::CompaniesController < SuperAdmin::SuperAdminController
 		@companies = Company.all
 	end
 
+	def new
+		@company = Company.new
+	end
+
+	def create
+		@company = Company.create(company_parameters)
+		if @company.save
+			redirect_to super_admin_company_path(@company)
+		else
+			render "new"
+		end
+	end
+
+	def show
+		@company = Company.find_by_id(params[:id])
+	end
+
+	def destroy
+		@company = Company.find_by_id(params[:id])
+		@company.destroy
+	end
+
+
+	private
+
+	def company_parameters
+		params.require(:company).permit(:name, :address, :phone, :max_admin, :is_active)
+	end
+
 end
